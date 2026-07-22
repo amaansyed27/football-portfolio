cae1a7a0e00ffece036093af04790c9e7498f52f
Amaan Syed <amaansyed27@gmail.com>
2026-07-23 02:32:46 +0530

    Load finale visual layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
 import './styles/rebuild.css'
+import './styles/finale.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
